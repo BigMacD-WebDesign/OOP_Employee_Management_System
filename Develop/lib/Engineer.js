a2c4fa88fc2c92c7cfc416cc8c58d9feb4390446
Scott MacDonald <scottmacd88@yahoo.com>
2020-05-04 16:56:24 -0700

    Added backend code to pass tests.

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee.js");
+
+class Engineer extends Employee {
+    constructor (name, email, id, github) {
+        super (name, email, id);
+        this.github = github;
+    }
+
+    getRole() {
+        return "Engineer";
+    }
+    getGithub() {
+        return this.github;
+    }
+}
+
+module.exports = Engineer;
